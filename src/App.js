56c7e135c118d5da7492434bc192d6ce711f0149
sunahhlee <sunah.lee1999@gmail.com>
2019-03-10 14:53:46 -0700

    current location enabled

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 // import {GoogleApiComponent} from 'google-maps-react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import { InfoWindow, Marker } from 'google-maps-react';
+import CurrentLocation from './Map';
+
 
 import ReactDOM from 'react-dom';
 // import * as ReactDOM from 'react-dom'
@@ -17,7 +19,7 @@ export class MapContainer extends Component {
         selectedPlace: {}
     };
 
-    onMarkerClick = (props, marker, e) =>
+    onMarkerClick = (props, marker, m2, e) =>
         this.setState({
             selectedPlace: props,
             activeMarker: marker,
@@ -28,7 +30,7 @@ export class MapContainer extends Component {
         if (this.state.showingInfoWindow === true) {
             this.setState({
                 showingInfoWindow: false,
-                activeMarker: null
+                activeMarker: null,
             });
         }
     }
@@ -39,19 +41,31 @@ export class MapContainer extends Component {
           height: '100%'
         };
         return (
-            <Map
-                google={this.props.google}
-                style={styles}
-                zoom = {14}
-                initialCenter={{
-                    lat: 37.871295,
-                    lng: -122.260314
-                }}
-            >
+            // <Map
+            //     google={this.props.google}
+            //     style={styles}
+            //     zoom = {14}
+            //     // initialCenter={{
+            //     //     lat: 37.871295,
+            //     //     lng: -122.260314
+            //     // }}
+            // >
+
+            <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
                 <Marker
                 onClick={this.onMarkerClick}
-                name={'UC Berkeley'}
+                name={'current location'}
                 />
+
+                <Marker
+                // onClick={this.onMarkerClick}
+                // name={'Test Marker'}
+                // position={{
+                //     lat: 37.875,
+                //     lng: -122.260314
+                // }}
+                />
+
                 <InfoWindow
                   marker={this.state.activeMarker}
                   visible={this.state.showingInfoWindow}
@@ -61,67 +75,15 @@ export class MapContainer extends Component {
                         <h2>{this.state.selectedPlace.name}</h2>
                   </div>
                 </InfoWindow>
-            </Map>
+
+            </CurrentLocation>
+
+            // </Map>
         );
     }
 }
 
 export default GoogleApiWrapper({
   // apiKey: 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
-  apiKey: 'AIzaSyBnOC2cYnLyaaYXtnd_IEQWZLkqvg0tqoE'
+  apiKey: 'AIzaSyDfaCamdV4CSw1jBTG8NZeem0YG6kguM3s'
 })(MapContainer);
-
-//
-// export class Container extends React.Component {
-//   render() {
-//     const style = {
-//         width: '100vw',
-//         height: '100vh'
-//     }
-//     if (!this.props.loaded) {
-//       return <div>Loading...</div>
-//     }
-//     return (
-//         <div style={style}>
-//           <Map google={this.props.google}/>
-//         </div>
-//     )
-//   }
-// }
-//
-// export class Map extends React.Component {
-//     componentDidUpdate(prevProps, prevState) {
-//         if (prevProps.google !== this.props.google) {
-//             this.loadMap();
-//         }
-//     }
-//
-//     componentDidMount() {
-//         this.loadMap();
-//     }
-//
-//     loadMap() { //function called after component rendered
-//         if (this.props && this.props.google) { // Google API is available to use
-//             const {google} = this.props;
-//             const maps = google.maps;
-//             const mapRef = this.refs.map;
-//             const node = ReactDOM.findDOMNode(mapRef);
-//             let zoom = 14;
-//             let lat = 37.774929;
-//             let lng = -122.419416;
-//             const center = new maps.LatLng(lat, lng);
-//             const mapConfig = Object.assign({}, {
-//                 center: center,
-//                 zoom: zoom
-//             })
-//             this.map = new maps.Map(node, mapConfig);
-//             // arguments are DOM node and config. object that creates map
-//         }
-//     }
-//     render() {
-//         return (
-//             <div ref='map'>
-//             </div>
-//         )
-//     }
-// }
