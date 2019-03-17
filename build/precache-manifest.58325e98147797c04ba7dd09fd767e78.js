7b407b87c12c868e9bf261bc97e6be1e79b5d78a
sunahhlee <sunah.lee1999@gmail.com>
2019-03-17 16:44:28 -0700

    this version build deploys

---
+++ build/precache-manifest.58325e98147797c04ba7dd09fd767e78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "54c82da6f38608dd0c88",
+    "url": "/static/js/main.54c82da6.chunk.js"
+  },
+  {
+    "revision": "f1251730ff3d48bb40ff",
+    "url": "/static/js/1.f1251730.chunk.js"
+  },
+  {
+    "revision": "54c82da6f38608dd0c88",
+    "url": "/static/css/main.4d8c0568.chunk.css"
+  },
+  {
+    "revision": "3f793ff2063dcd3b9ca3f4cac2282ac4",
+    "url": "/index.html"
+  }
+];
