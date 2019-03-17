075ab8412b9a7c7ef8bf63292c3270adddd4d172
Jason Ma <jason358ma@gmail.com>
2019-03-17 12:58:42 -0700

    random files to add

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // This is the file for the backend services
 const express = require('express');
-const favicon = require('express-favicon');
+const favicon = require('serve-favicon'); // require('express-favicon');
 const firebase = require("firebase");
 const path = require('path');
 const port = process.env.PORT || 8080;
@@ -16,13 +16,14 @@ const config = {
 };
 firebase.initializeApp(config);
 
-app.listen(port);
-
 const database = firebase.database();
+const auth = firebase.auth();
+app.use(express.json());
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
+// body-parser already included in express (https://stackoverflow.com/questions/11625519/how-to-access-the-request-body-when-posting-using-node-js-and-express)
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json()); // support json encoded bodies
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
@@ -32,15 +33,23 @@ app.get('/ping', function (req, res) {
     console.log('ping?');
     return res.send('pong');
 });
+// TODO: design and set up account structures in Firebase
+
+// TODO: Figure out how to have frontend send start and dest coordinates to be stored in user data on backend (HTTPS POST request?)
 app.post('/post', function (req, res) {
-    database.ref('users').set({
+    const ref = database.ref('users');
+    const uid = auth.currentUser.uid;
+    ref.child(uid).set({
         // https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
         age: req.body.age,
-        email: req.body.email,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        password: req.body.password,
-        username: req.body.username
+        // email: req.body.email, //on firebase auth
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        // username: req.body.username, //on firebase auth
+        startLat: req.body.startLat,
+        startLong: req.body.startLong,
+        destLat: req.body.destLat,
+        destLong: req.body.destLong
     }, function (error) {
         if (error) {
             return res.send('post failed! Error is:' + error);
@@ -50,10 +59,16 @@ app.post('/post', function (req, res) {
     });
     // return res.send('post request');
 });
-// app.get('/test', function (req, res) {
-//     return res.send({'test': 'test message here!'});
-// });
+
+// TODO: Firebase send personal profile info to user after logging in so that it can be displayed? (firstname, lastname, age)
+app.get('/test', function (req, res) {
+    database.ref("users/" + req.)
+    return res.send({'test': 'test message here!'});
+});
+
 // app.get('/', function (req, res) {
 //     console.log('GET root');
 //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
+
+app.listen(port);
